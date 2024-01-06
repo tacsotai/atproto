5cb738decc85bc4f4065d8fa366d1520902e9ffe
Maruyama Takeshi <tac@sotai.co>
2024-01-06 18:12:27 +0900

    local setting2

--- packages/pds/src/basic-routes.ts
+++ packages/pds/src/basic-routes.ts
@@ -8,18 +8,11 @@ export const createRouter = (ctx: AppContext): express.Router => {
   router.get('/', function (req, res) {
     res.type('text/plain')
     res.send(
-      'This is an AT Protocol Personal Data Server (PDS): https://github.com/bluesky-social/atproto\n\nMost API routes are under /xrpc/',
+      'Under construction',
     )
   })
 
-  router.get('/robots.txt', function (req, res) {
-    res.type('text/plain')
-    res.send(
-      '# Hello!\n\n# Crawling the public API is allowed\nUser-agent: *\nAllow: /',
-    )
-  })
-
-  router.get('/xrpc/_health', async function (req, res) {
+  router.get('/xrpc/_health2', async function (req, res) {
     const { version } = ctx.cfg
     try {
       await sql`select 1`.execute(ctx.db.db)
